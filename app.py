68ecd555a4183d4a26689bf1f3b445b5757e68b1
Bovine Hero <bhero@Bovines-HeroBook-Pro.local>
2023-02-25 10:40:33 +0000

    changing resume references and assets to personas

--- app.py
+++ app.py
@@ -18,46 +18,27 @@ def index():
 @app.route("/about")
 def about():
     data = []
-    with open("data/company.json", "r") as json_data:
+    with open("data/about.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("about.html", page_title="About", company=data)
-
-
-@app.route("/about/<member_name>")
-def about_member(member_name):
-    member = {}
-    with open("data/company.json", "r") as json_data:
-        data = json.load(json_data)
-        for obj in data:
-            if obj["url"] == member_name:
-                member = obj
-    return render_template("member.html", member=member)
-
-
-@app.route("/contact", methods=["GET", "POST"])
-def contact():
-    if request.method == "POST":
-        flash("Thanks {}, we have received your message!".format(
-            request.form.get("name")))
-    return render_template("contact.html", page_title="Contact")
+    return render_template("about.html", page_title="About", about_data=data)
 
 
 @app.route("/personas")
 def personas():
     data = []
-    with open("data/company.json", "r") as json_data:
+    with open("data/personas.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("personas.html", page_title="The Personas", company=data)
+    return render_template("personas.html", page_title="The Personas", personas=data)
 
-@app.route("/persona/<member_name>")
-def about_persona(member_name):
-    member = {}
-    with open("data/company.json", "r") as json_data:
+@app.route("/persona/<persona_name>")
+def about_persona(persona_name):
+    persona = {}
+    with open("data/personas.json", "r") as json_data:
         data = json.load(json_data)
         for obj in data:
-            if obj["url"] == member_name:
-                member = obj
-    return render_template("member.html", member=member)
+            if obj["url"] == persona_name:
+                persona = obj
+    return render_template("member.html", persona=persona)
 
 
 if __name__ == "__main__":
